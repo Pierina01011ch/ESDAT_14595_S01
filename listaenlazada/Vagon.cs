9d5e6e77b28cf29af13791dfbbe40f6e7fbfc0e0
bojorquezja <bojorquez.ja@gmail.com>
2026-03-24 14:09:50 -0500

    primer commit

---
+++ listaenlazada/Vagon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaenlazada
+{
+    internal class Vagon
+    {
+        public Vagon Sig { get; set; }
+        
+        public int Dato { get; set; }
+
+        public Vagon(int dato)
+        {
+            Sig = null;
+            Dato = dato;
+        }
+    }
+}
